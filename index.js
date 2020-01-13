619144feaf5be9646559bf10d2317fe18962ef69
amymcgowan <amymcgowan624@gmail.com>
2020-01-13 12:53:49 -0500

    Set up sample data in mongo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var express             = require("express");
 // APP CONFIG
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect("mongodb://localhost/restful-blog-app");
+mongoose.connect("mongodb://localhost/restful-art-app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,7 +18,19 @@ var tutorialSchema = new mongoose.Schema({
     title: String,
     image: String,
     videoID: String,
+    videoLink: String,
     colors: String
 })
 
 var Tutorial = mongoose.model("Tutorial", tutorialSchema);
+
+// CREATE SAMPLE DATA
+
+// Tutorial.create({
+//     title: "Rustic Doorway",
+//     image: "https://img.youtube.com/vi/W9Pir8sgaA/maxresdefault.jpg",
+//     videoID: "W9Pir8sgaA",
+//     videoLink: "https://www.youtube.com/watch?v=W9Pir8sgaA",
+//     colors: "Dandelion Yellow, Fuschia, Azure Blue"
+// })
+
